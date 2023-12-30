ef1ad0d39b6f03c37b24fec3c821581a205bf095
Naim <mdnaim01303202218@gmail.com>
2023-12-30 20:46:43 +0600

    post operation done

--- Master Curd/server/index.js
+++ Master Curd/server/index.js
@@ -1,31 +1,58 @@
-const express = require('express');
-const { MongoClient } = require('mongodb');
-require('colors')
+const express = require("express");
+const { MongoClient } = require("mongodb");
+require("colors");
+const cors = require('cors');
 
-const app = express()
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
 
 const port = process.env.PORT || 5000;
-const uri ='mongodb+srv://Naim:Naim123@curd.gploutp.mongodb.net/?retryWrites=true&w=majority';
-const client = new MongoClient(uri)
+const uri =
+  "mongodb+srv://Naim:Naim123@curd.gploutp.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
 
-async function run(){
-   try {
+async function DBconnected() {
+  try {
     await client.connect();
     console.log(`mongoDB connected`.cyan.bold);
-
-    const Product = client.db('foodPanda').collection('products')
-    const User = client.db('foodPanda').collection('users')
-
-    Product.insertOne({name: "Pizza"});
-
-   } catch (error) {
-    console.log(error.name.red, error.message.bold.red); 
-   }
+  } catch (error) {
+    console.log(error.name.red, error.message.bold.red);
+  }
 }
-run()
+DBconnected();
+const Product = client.db("foodPanda").collection("products");
+const User = client.db("foodPanda").collection("users");
 
+//endpoint
+app.post("/product", async (req, res) => {
+  try {
+    const result = await Product.insertOne(req.body);
+    console.log('new user', result);
 
+    if (result.insertedId) {
+      res.send({
+        success: true,
+        message: `Successfully created the ${req.body.name} with id ${result.insertedId}`,
+      });
+    } else {
+      res.send({
+        success: false,
+        error: "Couldn't create the product",
+      });
+    }
+  } catch (error) {
+    console.log(error.name.bgRed, error.message.bold);
+    res.send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
 
-app.listen(port , ()=>{
-    console.log(`app is running on ${port}`.cyan.bold);
-})
+app.listen(port, () => {
+  console.log(`app is running on ${port}`.cyan.bold);
+});
