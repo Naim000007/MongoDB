408b096989c8a5a8d455ca4196d192fae72f8e50
Naim <mdnaim01303202218@gmail.com>
2023-12-30 15:54:56 +0600

    mongodb connected

--- Master Curd/server/index.js
+++ Master Curd/server/index.js
@@ -5,8 +5,24 @@ require('colors')
 const app = express()
 
 const port = process.env.PORT || 5000;
+const uri ='mongodb+srv://Naim:Naim123@curd.gploutp.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(uri)
 
+async function run(){
+   try {
+    await client.connect();
+    console.log(`mongoDB connected`.cyan.bold);
 
+    const Product = client.db('foodPanda').collection('products')
+    const User = client.db('foodPanda').collection('users')
+
+    Product.insertOne({name: "Pizza"});
+
+   } catch (error) {
+    console.log(error.name.red, error.message.bold.red); 
+   }
+}
+run()
 
 
 
